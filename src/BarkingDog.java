900313ba81f9535674e0838b92ed78592ae50e40
lloydkev000 <lloydkevin631@gmail.com>
2024-09-19 08:33:16 -0400

    All Done!

--- src/BarkingDog.java
+++ src/BarkingDog.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class BarkingDog {
+public class BarkingDog {
+    public static void main(String[] args) {
+        System.out.println(shouldWake(true, 1));
+        System.out.println(shouldWake(false, 2));
+        System.out.println(shouldWake(true, 8));
+        System.out.println(shouldWake(true, -1));
+    }
+
+    private static boolean shouldWake(boolean isBarking, int hourOfDay) {
+        if( hourOfDay < 0 || hourOfDay > 23) //if statements check for less than 0 or greater than 23:
+            return false;
+
+        return hourOfDay < 8 || hourOfDay > 22; //Understood after doing the first if statement.
+    }
+
+
 }
